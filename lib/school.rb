10d18ad2fad81690828e28e041b231a5f8dc910b
Jesse Smokovich <jessesmokovich@yahoo.com>
2019-04-23 21:54:52 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -16,7 +16,6 @@ class School
     roster[student_grade]
   end
 
-  # this method should arrange the students in each grade by alphabetical order
   def sort
     sorted = {}
     roster.each do |grade, students|
